d5b2792b3aa04b6d08a5104c8996ef8ec531d5ad
mumu <617958070@qq.com>
2020-06-21 22:23:54 +0800

    相交链表

---
+++ src/LinkList/分隔链表/Solution.java
@@ -0,0 +1,13 @@
+package LinkList.分隔链表;
+
+import DataStructure.ListNode;
+
+public class Solution {
+    /**
+     * https://leetcode-cn.com/problems/partition-list/
+     * 1： 分成三部分 小于等于  本身  大于  然后再组合成一个链表
+     */
+    public ListNode partition(ListNode head, int x) {
+        
+    }
+}
--- src/LinkList/相交链表/Solution.java
+++ src/LinkList/相交链表/Solution.java
@@ -5,9 +5,17 @@ import DataStructure.ListNode;
 public class Solution {
     /**
      * https://leetcode-cn.com/problems/intersection-of-two-linked-lists/
-     *
+     * 遍历两个链表然后到最后一个节点分别指向对方的链表。
      */
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-
+        if (headA == null || headB == null) return null;
+        ListNode curA = headA;
+        ListNode curB = headB;
+        while (curA != curB) {
+            //如果a为空就指向b链表 ，如果 b空就指向 a链表
+            curA = curA == null ? headB : curA.next;
+            curB = curB == null ? headA : curB.next;
+        }
+        return curA;
     }
 }
