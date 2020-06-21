90bed4a81acedb414a124286bdd8f8bcddbd3b1a
木木 <617958070@qq.com>
2020-06-21 14:01:35 +0800

    -功能[ 颜色分类 ] -备注[ ]

---
+++ src/LinkList/部分排序/Solution.java
@@ -0,0 +1,13 @@
+package LinkList.部分排序;
+
+public class Solution {
+    /**
+     * https://leetcode-cn.com/problems/sub-sort-lcci/
+     * 寻找逆序对
+     */
+    public int[] subSort(int[] array) {
+
+    }
+
+
+}
---
+++ src/LinkList/颜色分类/Solution.java
@@ -0,0 +1,45 @@
+package LinkList.颜色分类;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Solution {
+    /**
+     * https://leetcode-cn.com/problems/sort-colors/
+     * 原地排序，常数空间。
+     */
+    public void sortColors(int[] nums) {
+        int i = 0;
+        int left = 0;
+        int right = nums.length - 1;
+        while (i <= right) {
+            int v = nums[i];
+            if (v == 0) {
+                exchange(nums, i, left);
+                i++;
+                left++;
+            } else if (v == 1) {
+                i++;
+            } else {
+                exchange(nums, i, right);
+                right--;
+            }
+        }
+    }
+
+    public void exchange(int[] nums, int a, int b) {
+        if (a == b) {
+            return;
+        }
+        int tmp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = tmp;
+    }
+
+    /**
+     * 双指针做法、三指针
+     *
+     */
+
+
+}
