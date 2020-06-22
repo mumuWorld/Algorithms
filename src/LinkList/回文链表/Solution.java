b3bf554c0e427e39368c30613d49f16981fbffaf
mumu <617958070@qq.com>
2020-06-22 08:37:02 +0800

    回文链表未完成

---
+++ src/LinkList/回文链表/Solution.java
@@ -0,0 +1,14 @@
+package LinkList.回文链表;
+
+import DataStructure.ListNode;
+
+public class Solution {
+    /**
+     * https://leetcode-cn.com/problems/palindrome-linked-list/
+     * 中间节点： 下一个节点是后半部分节点的开始。
+     * 1 找到中间节点， 2 翻转右半部分链表  3 依次对比左边和右边
+     */
+    public boolean isPalindrome(ListNode head) {
+
+    }
+}
