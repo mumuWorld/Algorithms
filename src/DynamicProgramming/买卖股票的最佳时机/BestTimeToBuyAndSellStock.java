e4e5d7daff468f9b5096471da9892f6d1b6eff8e
mumu <617958070@qq.com>
2020-07-09 23:38:06 +0800

    初学动态规划

---
+++ src/DynamicProgramming/买卖股票的最佳时机/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,13 @@
+package DynamicProgramming.买卖股票的最佳时机;
+
+public class BestTimeToBuyAndSellStock {
+    /**
+     * https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock/
+     *
+     * @param prices
+     * @return
+     */
+    public int maxProfit(int[] prices) {
+        
+    }
+}
--- src/String/最长无重复子串/ZuiChangWuChongFuZiChuan.java
+++ src/String/最长无重复子串/ZuiChangWuChongFuZiChuan.java
@@ -7,12 +7,11 @@ public class ZuiChangWuChongFuZiChuan {
      * 无重复字符的最长子串
      * https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/
      * 滑动窗口法
-     * @param s
-     * @return
      */
         public int lengthOfLongestSubstring(String s) {
             if (s == null) return 0;
             if (s.length() < 2) return s.length();
+            //左边一个标志位
             int ans = 0, left = 0;
             HashMap<Character, Integer> map = new HashMap<>(); // current index of character
             for (int i = 0; i < s.length(); i++) {
@@ -20,6 +19,7 @@ public class ZuiChangWuChongFuZiChuan {
                     int index = map.get(s.charAt(i));
                     left = Math.max(left, index + 1);
                 }
+                //包不包含都push
                 map.put(s.charAt(i),i);
                 ans = Math.max(ans, i - left + 1);
             }
