eb8e76a1f632afde6343513ab5a7108871d69acb
mumu <617958070@qq.com>
2020-07-17 16:39:53 +0800

    移动零

--- src/DFS/路径总和2/LuJingZongHe2.java
+++ src/DFS/路径总和2/LuJingZongHe2.java
@@ -2,10 +2,30 @@ package DFS.路径总和2;
 
 import DataStructure.TreeNode;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LuJingZongHe2 {
     public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        List<List<Integer>> list = new ArrayList<>();
+        if (root == null) return list;
+        dfs(root, sum, new ArrayList<>(),list);
+        return list;
+    }
 
+    private void dfs(TreeNode root,int sum, List<Integer> result,List<List<Integer>> results) {
+        if (root == null) return;
+        result.add(root.val);
+        sum -= root.val;
+        //叶子节点 执行完不能return 要进行remove一次。
+        if (root.left == null && root.right == null) {
+            if (sum == 0) {
+                results.add(new ArrayList<>(result));
+            }
+        } else {
+            dfs(root.left, sum, result, results);
+            dfs(root.right, sum, result, results);
+        }
+        result.remove(result.size() - 1);
     }
 }
