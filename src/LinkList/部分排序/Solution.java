249949f311135e11345d9a380a9490bdcbbc5e2e
木木 <617958070@qq.com>
2020-07-21 12:13:00 +0800

    -功能[ 先提交] -备注[ ]

--- src/LinkList/部分排序/Solution.java
+++ src/LinkList/部分排序/Solution.java
@@ -4,9 +4,31 @@ public class Solution {
     /**
      * https://leetcode-cn.com/problems/sub-sort-lcci/
      * 寻找逆序对
+     * 从左到右 ，
      */
     public int[] subSort(int[] array) {
-
+        if (array.length < 1) { return new int[] {-1,-1}; }
+        int i = 0;
+        int j = array.length - 1;
+        int right = -1;
+        int left = -1;
+        int min = array[j--];
+        int max = array[i++];
+        for (; i < array.length ; i++ ,j-- ) {
+            int leftV = array[i];
+            int rightV = array[j];
+            if (leftV < max) {  //从左向右遍历
+                left = i;
+            } else {
+                max = leftV;
+            }
+            if (rightV > min) { //从右向左遍历
+                right = j;
+            } else {
+                min = rightV;
+            }
+        }
+        return new int[] {right,left};
     }
 
 
