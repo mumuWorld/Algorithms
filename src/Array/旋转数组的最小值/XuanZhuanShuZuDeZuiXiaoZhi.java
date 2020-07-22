8064161a7837168d964af7b2a537011603f26a10
木木 <617958070@qq.com>
2020-07-22 20:10:30 +0800

    -功能[ 哈希 ] -备注[ ]

--- src/Array/旋转数组的最小值/XuanZhuanShuZuDeZuiXiaoZhi.java
+++ src/Array/旋转数组的最小值/XuanZhuanShuZuDeZuiXiaoZhi.java
@@ -1,6 +1,11 @@
 package Array.旋转数组的最小值;
 
+import java.util.HashSet;
+
 public class XuanZhuanShuZuDeZuiXiaoZhi {
+    /**
+     * https://leetcode-cn.com/problems/xuan-zhuan-shu-zu-de-zui-xiao-shu-zi-lcof/
+     */
     public int minArray(int[] numbers) {
         if (numbers == null) return 0;
         if (numbers.length == 1) return numbers[0];
@@ -15,4 +20,24 @@ public class XuanZhuanShuZuDeZuiXiaoZhi {
         }
         return numbers[0];
     }
+
+    /**
+     * https://leetcode-cn.com/problems/jewels-and-stones/comments/
+     * @param J
+     * @param S
+     */
+    public int numJewelsInStones(String J, String S) {
+        HashSet set = new HashSet();
+        for (char ch: J.toCharArray()) {
+            set.add(ch);
+        }
+        int sum = 0;
+        for (char ch: S.toCharArray()) {
+            if (set.contains(ch)) {
+                sum++;
+            }
+        }
+        return sum;
+    }
+
 }
