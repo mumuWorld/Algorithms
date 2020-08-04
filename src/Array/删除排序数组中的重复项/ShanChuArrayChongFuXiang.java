b5f37a0d3e440f4ec91b798be9a8e7e02faa650f
mumu <617958070@qq.com>
2020-08-04 23:42:23 +0800

    删除重复

---
+++ src/Array/删除排序数组中的重复项/ShanChuArrayChongFuXiang.java
@@ -0,0 +1,20 @@
+package Array.删除排序数组中的重复项;
+
+import DataStructure.ArrayTools;
+
+public class ShanChuArrayChongFuXiang {
+    public int removeDuplicates(int[] nums) {
+        if (nums == null) return 0;
+        if (nums.length < 2) return nums.length;
+        int result = 0;
+        int pre = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[pre]) {
+                pre++;
+                nums[pre] = nums[i];
+
+            }
+        }
+        return pre +1;
+    }
+}
