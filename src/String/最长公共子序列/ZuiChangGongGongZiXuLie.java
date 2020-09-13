2cef5de1bebf076b47bc26d14b49d16ae2ed2556
mumu <617958070@qq.com>
2020-09-13 22:04:54 +0800

    优化成1位数组

--- src/String/最长公共子序列/ZuiChangGongGongZiXuLie.java
+++ src/String/最长公共子序列/ZuiChangGongGongZiXuLie.java
@@ -25,12 +25,36 @@ public class ZuiChangGongGongZiXuLie {
                 } else {
                     dp[i][j] = Math.max(dp[i][j-1],dp[i-1][j]);
                 }
-
             }
         }
         return dp[chars.length][chars2.length];
     }
 
+    /**
+     * 优化  一位数组
+     * @param text1
+     * @param text2
+     * @return
+     */
+    public int longestCommonSubsequence_2(String text1, String text2) {
+        if (text1.length() < 1 || text2.length() < 1) return 0;
+        char[] chars = text1.toCharArray();
+        char[] chars2 = text2.toCharArray();
+        int[] dp = new int[chars2.length + 1];
+        for (int i = 1; i <= chars.length; i++) {
+            int cur = 0;
+            for (int j = 1; j <= chars2.length; j++) {
+                int leftTop = cur; //上一个topleft
+                cur = dp[j];  //保存本次的 topleft。
+                if (chars[i -1] == chars2[j - 1]) {
+                    dp[j] = leftTop + 1;
+                } else {
+                    dp[j] = Math.max(dp[j-1], dp[j]);
+                }
+            }
+        }
+        return dp[chars2.length];
+    }
 //    public int longestCommonSubsequence(int[] text1, int[] text2) {
 //
 //    }
