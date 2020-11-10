3593b322a3a764bb89c96ff73c94d2a9ee0f7014
木木 <617958070@qq.com>
2020-11-10 20:48:24 +0800

    -功能[ 窗口算法 ] -备注[ ]

---
+++ src/String/最小覆盖子串.java
@@ -0,0 +1,60 @@
+package String;
+
+import java.util.HashMap;
+
+public class 最小覆盖子串 {
+    /**
+     * https://leetcode-cn.com/problems/minimum-window-substring/
+     * @return
+     */
+    public String minWindow(String s, String t) {
+        if (t == null || t.length() == 0) return  s;
+        int needValue = 0;
+        //最小字符串记录
+        int start = 0, end = 0, len = Integer.MAX_VALUE;
+        int left = 0, right = 0;
+        HashMap <Character,Integer> window = new HashMap<Character, Integer>();
+        HashMap <Character,Integer> need = new HashMap<Character, Integer>();
+        for (Character cc : t.toCharArray()) {
+            Integer tmp = need.getOrDefault(cc, 0);
+            need.put(cc, tmp + 1);
+        }
+        System.out.println("test1->"+need.size()+ "-leng-" + s.length());
+        for (int i = 0; i < s.length(); i++) {
+            Character cur = s.charAt(i);
+            right = i + 1;
+
+            if (need.containsKey(cur)) {
+                //将单钱字符入窗口
+                Integer tmp = window.getOrDefault(cur, 0);
+                window.put(cur, tmp + 1);
+                if (window.get(cur) == need.get(cur)) {
+                    needValue += 1;
+                }
+            }
+            while (needValue == need.size()) {
+                if (right - left < len) {
+                    start = left;
+                    len = right - left;
+                }
+                System.out.println("test5->" + start);
+
+                //准备移除leftC
+                Character leftC = s.charAt(left);
+                left += 1;
+
+                if (need.containsKey(leftC)) {
+                    if (window.get(leftC) == need.get(leftC)) {
+                        needValue -= 1;
+                    }
+                    //将单字符移除窗口
+                    Integer leftT = window.getOrDefault(leftC, 0);
+                    window.put(leftC, leftT == 0 ?  0 : leftT - 1);
+                }
+            }
+        }
+        System.out.println("test3->" + start+ "->" + len);
+        System.out.println("test4->" + (len == Integer.MAX_VALUE));
+        return len == Integer.MAX_VALUE ? "" : s.substring(start, start + len);
+    }
+}
