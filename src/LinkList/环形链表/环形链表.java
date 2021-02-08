41c9ea98b409b2ce276f8f2171c7800d2b5d521e
木木 <617958070@qq.com>
2021-02-08 15:57:08 +0800

    -功能[ 唤醒链表2 ] -备注[ ]

--- src/LinkList/环形链表/环形链表.java
+++ src/LinkList/环形链表/环形链表.java
@@ -13,4 +13,27 @@ public class 环形链表 {
         }
         return false;
     }
+
+    /**  https://leetcode-cn.com/problems/linked-list-cycle-ii/
+     *  环形链表2
+     *  快 比慢 多走了 n圈
+     * @param head
+     * @return
+     */
+    public ListNode detectCycle(ListNode head) {
+        ListNode fast = head, slow = head;
+        while (true) {
+            if (fast == null || fast.next == null) return  null;
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) break;
+        }
+        fast = head;
+        while (fast != slow) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return fast;
+    }
+
 }
